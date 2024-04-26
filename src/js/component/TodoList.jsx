1e86b2a984d8451c68bdea3720309a24f0361e8d
Victoria González Rodríguez <159072629+Vickygonrod@users.noreply.github.com>
2024-04-26 02:00:07 +0000

    ToDoList sin CheckBox

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,49 @@
+import React , {useState} from "react";
+
+export const ToDoList = () => {
+    const [task , setTask] = useState('');
+    const [list , setList] = useState(['Wake up']);
+    
+
+   
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (task.trim() !== '') {
+            setList([...list, task]);
+            setTask('');
+          } else {
+            setTask('');
+          }
+    }
+
+    const deletetask = (item) => {
+        console.log(item)
+        setList(list.filter((element) => element !== item))
+      }
+   
+    return(
+ <>
+    <div className="container col-10 col-sm-8 col-md-6">
+       <form onSubmit={handleSubmit}>
+            <label htmlFor="exampleFormControlInput1"><h1>To-Do list</h1></label>
+            <input type="text" className="form-control container col-6 sm-col-4" id="exampleFormControlInput1" 
+            value={task} onChange={(event) => setTask(event.target.value)} />
+        </form>
+        <ul className="list-group mt-2">       
+          {list.map((item, id) => <li key={id} className="list-group-item d-flex justify-content-between hidden-icon">
+            {item}
+            <span id="trash" onClick={() => deletetask(item)}>
+              <i className="fas fa-trash text-danger"></i>
+            </span>
+                       
+            </li>)}
+            
+          <li className="list-group-item text-end bg-light fw-lighter">
+            {list.length} items
+          </li>
+        </ul>
+  
+        </div>
+ </>
+    )
+}
